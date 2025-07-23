0cdac20caea8102912b6f5e894653cc8638faf71
Kit Bagasol <mjbolwar@gmail.com>
2025-07-24 04:56:25 +0800

    added some style to the html
    change grid button is now working

--- script.js
+++ script.js
@@ -1,22 +1,46 @@
 const container = document.querySelector(".container");
 const boxes = document.querySelector(".boxes");
 const boxHolder = document.querySelector(".boxHolder");
+const changeGrid = document.querySelector(".changeGrid");
 
 
-let grid = 16;
+let gridSize = 16;
 
-for (let i = 0; i < grid; i++) {
+function createGrid (size) {
+    
+    container.innerHTML = "";
+
+    for (let i = 0; i < size; i++) {
     const div = document.createElement("div");
     div.setAttribute("class", "boxHolder");
     container.appendChild(div);
 
-    for (let j = 0; j < grid; j++) {
+    for (let j = 0; j < size; j++) {
         const innerBox = document.createElement("div");
         innerBox.setAttribute("class", "boxes");
         div.appendChild(innerBox);  
-    }
-        
+
+        innerBox.addEventListener("mouseenter", () => {
+            innerBox.style.backgroundColor = "#727D73";
+        });
+    };
 };
+};
+
+createGrid(gridSize);
+
+changeGrid.addEventListener("click", () => {
+    const input = prompt("Enter a number from 2-100", );
+    const newSize = parseInt(input);
+
+    if (newSize >= 2 && newSize <= 100) {
+        gridSize = newSize;
+        createGrid(gridSize);
+        console.log(`Your number is: ${gridSize}`);
+    } else {
+        alert("Please enter a valid number");
+    };
+});
 
 
 
